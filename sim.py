b13f18056038431d0a50fd02675ddb6687d29864
Rosa-Lavelle-Hill <rosaellenlavellehill@gmail.com>
2023-12-06 14:36:43 +0100

    add LIME

--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
 from random import random
-
-import seaborn as sns
+import lime
+import lime.lime_tabular
 import numpy as np
 import pandas as pd
 import shap as shap
@@ -22,7 +22,7 @@ from PyALE import ale
 # supported model classes for pred_model: "enet" for elastic net regression, "lasso" for lasso regression,
 # ..."tree" for a decision tree, and "rf" for a random forest
 
-pred_model = "rf" # string defining the prediction model to use (see above for alternatives)
+pred_model = "tree" # string defining the prediction model to use (see above for alternatives)
 n_samples = 100 # number of samples in generated data
 n_features = 5 # number of features (or "independent variables") in generated data
 mean = 0 # mean of generated data
@@ -33,15 +33,19 @@ test_size = 0.5 # ratio of training:test data
 cv = 5 # number of cross-validation splits
 scoring = "r2" # scoring used for both the training and the testing: 'r2' is prediction R-squared, for other options, see: https://scikit-learn.org/stable/modules/model_evaluation.html
 permutations = 10 # number of permutations in permutation importance calculations
-force_plot_data_instance_num = 0 # the row number indicating which person in the data to create a SHAP force plot for
+explain_data_instance_num = 0 # the row index indicating which instance in the data to create a local explanation for (used for SHAP and LIME)
 decimal_places = 2 # integer used for rounding
 seed = 93 # the random seed (used in the data generating process, splitting process, the model fitting process, and the permutation importance calculations)
-
+results_path = "Results/Interpretation/"
 # -------------------------- Run Simulations --------------------------
 
 if __name__ == '__main__':
     start_time = dt.datetime.now()
-    print("Running analysis for {} model...".format(pred_model))
+    if pred_model not in ["rf", "tree", "lasso", "enet"]:
+        print("please enter one of the regression or tree based models: 'rf', 'tree', 'lasso, or 'enet'")
+        breakpoint()
+    else:
+        print(f"Running analysis for {pred_model} model...")
 
     # -------------------------- Generate Data --------------------------
 
@@ -59,7 +63,7 @@ if __name__ == '__main__':
 
     X_feature_names = []
     for i in list(range(1, X.shape[1] + 1)):
-        n = "X{}".format(i)
+        n = f"X{i}"
         X_feature_names.append(n)
 
     # Predict y from X with fixed coefficients (b=1)
@@ -96,7 +100,7 @@ if __name__ == '__main__':
 
     grid_search.fit(X_train, y_train)
     best_params = grid_search.best_params_
-    print("best hyper-params from training: {}".format(best_params))
+    print(f"best hyper-params from training: {best_params}")
     model.set_params(**best_params)
     model.fit(X_train, y_train)
     cv_r2 = grid_search.best_score_
@@ -104,14 +108,15 @@ if __name__ == '__main__':
     # Predict OOS test data
     y_pred = model.predict(X_test)
     test_r2 = round(metrics.r2_score(y_test, y_pred), decimal_places)
-    print('Model performance on unseen test data: {} Prediction R2'.format(test_r2))
+    print(f'Model performance on unseen test data: {test_r2} Prediction R2')
 
     # ------------------------------------------ Explanations ------------------------------------------
 
     vars = X_feature_names.copy()
 
-    ## 1a) Permutation importance (a single imputation)
-    save_path = "Results/Interpretation/Permutation/"
+    ## 1 Permutation importance
+    # a) single permutation
+    save_path = results_path + "Permutation/"
     result = permutation_importance(model, X_test, y_test, n_repeats=1, # a single imputation
                                     random_state=seed, n_jobs=2, scoring=scoring)
     perm_importances_mean = result.importances_mean
@@ -119,9 +124,9 @@ if __name__ == '__main__':
     perm_imp_df = pd.DataFrame(dict)
     # flip so most important at top on graph
     perm_imp_df.sort_values(by="Importance", ascending=True, inplace=True, axis=0)
-    plot_permutation(perm_imp_df=perm_imp_df, save_path=save_path, save_name="{}_permutation".format(pred_model))
+    plot_permutation(perm_imp_df=perm_imp_df, save_path=save_path, save_name=f"{pred_model}_permutation")
 
-    # 1b) Plot multiple permutations with with variance bars:
+    # b) multiple permutations (plot with variance bars)
     result = permutation_importance(model, X_test, y_test, n_repeats=permutations,
                                     random_state=seed, n_jobs=2, scoring=scoring)
     plot_multiple_permutations(result=result, vars=vars, save_path=save_path,
@@ -129,7 +134,7 @@ if __name__ == '__main__':
 
     ## 2) Tree-based impurity importance
     if (pred_model == 'rf') or (pred_model == "tree"):
-        save_path = "Results/Interpretation/Impurity/"
+        save_path = results_path + "Impurity/"
         feature_importances = model.feature_importances_
 
         dict = {'Feature': vars, "Importance": feature_importances}
@@ -141,10 +146,10 @@ if __name__ == '__main__':
         # plot
         plot_impurity(impurity_imp_df=impurity_imp_df,
                       save_path=save_path,
-                      save_name="{}_impurity".format(pred_model))
+                      save_name=f"{pred_model}_impurity")
 
     ## 3) SHAP importance
-    save_path = "Results/Interpretation/SHAP/"
+    save_path = results_path + "SHAP/"
     if (pred_model == "rf") or (pred_model == "tree"):
         explainer = shap.TreeExplainer(model, feature_pertubation="tree_path_dependent")# "true to the data" approach (see Lundberg et al., 2020)
         shap_dict = explainer(X_test)
@@ -156,8 +161,6 @@ if __name__ == '__main__':
         shap_dict = explainer(X_test)
         shap_values = explainer.shap_values(X_test)
         shap_values_df = pd.DataFrame(shap_values, columns=vars)
-    else:
-        print("please enter one of the regression or tree based models: 'rf', 'tree', 'lasso, or 'enet'")
 
     # a) Plot summary "global" plots:
     plot_types = ["bar", "summary", "violin"]
@@ -165,17 +168,17 @@ if __name__ == '__main__':
         plot_SHAP(shap_dict, col_list=vars,
                   n_features=n_features, plot_type=plot_type,
                   save_path= save_path, title="SHAP importance (test set)",
-                  save_name="{}_shap_{}.png".format(pred_model, plot_type))
+                  save_name=f"{pred_model}_shap_{plot_type}.png")
         if plot_type == "summary":
             plot_SHAP(shap_dict, col_list=vars,
                       n_features=n_features, plot_type=None,
                       save_path= save_path, title="SHAP importance (test set)",
-                      save_name="{}_shap_{}.png".format(pred_model, plot_type))
+                      save_name=f"{pred_model}_shap_{plot_type}.png")
 
     # b) Example of SHAP local force plot for data instance i:
-    plot_SHAP_force(i=force_plot_data_instance_num, X_test=pd.DataFrame(X_test, columns=vars), model=model, save_path=save_path,
-                    save_name="{}_shap_local".format(pred_model), pred_model=pred_model,
-                    title="")
+    plot_SHAP_force(i=explain_data_instance_num, X_test=pd.DataFrame(X_test, columns=vars), model=model,
+                    save_path=save_path, save_name=f"{pred_model}_shap_local", pred_model=pred_model,
+                    title="local explanation")
 
     ## 4) Partial Dependence Plot (PDP)
     # select features to plot automatically based on permutation importance (most important)
@@ -184,16 +187,16 @@ if __name__ == '__main__':
     f2=perm_imp_df['Feature'].iloc[1]
     # first 2D, then 3D plot (on same figure):
     features = [f1, (f1, f2)]
-    save_path="Results/Interpretation/PDP/"
+    save_path= results_path + "PDP/"
     X_test = pd.DataFrame(X_test, columns=vars)
     plot_PDP(save_path=save_path, pred_model=pred_model, model=model, X_test=X_test, features=features)
 
     ## 5) Individual Conditional Expectation (ICE) plot
-    save_path="Results/Interpretation/ICE/"
+    save_path= results_path + "ICE/"
     plot_ICE(save_path=save_path, pred_model=pred_model, model=model, X_test=X_test, feature=f1)
 
     ## 6) Accumulated Local Effects (ALE) graph
-    save_path = "Results/Interpretation/ALE/"
+    save_path = results_path + "ALE/"
     grid = 50
     # a) one variable:
     ale_eff = ale(X=X_test, model=model, feature=[f1], grid_size=grid, include_CI=False)
@@ -202,15 +205,23 @@ if __name__ == '__main__':
     ale_eff_2D = ale(X=X_test, model=model, feature=[f1, f2], grid_size=grid)
     plt.savefig(save_path + f"{pred_model}_2D_ale.png")
 
-    ## 7) LIME
+    ## 7) Local Interpretable Model-agnostic Explanations (LIME)
+    save_path = results_path + "LIME/"
+    lime_explainer = lime.lime_tabular.LimeTabularExplainer(X_train, feature_names=vars, mode="regression",
+                                                            verbose=False, discretize_continuous=False)
+    X_instance = X_test.iloc[explain_data_instance_num, :]
+    exp = lime_explainer.explain_instance(data_row=X_instance, predict_fn=model.predict, num_features=len(vars))
+    # Create a plot from the explanation
+    fig = exp.as_pyplot_figure()
+    plt.tight_layout()
+    plt.savefig(save_path + f'{pred_model}_lime.png')
 
     ## 8) Print decision tree structure (here, for visual purposes only)
     if (pred_model == 'rf') or (pred_model == "tree"):
-        save_path = "Results/Interpretation/Tree/"
+        save_path = results_path + "Tree/"
         # pre-define depth parameter (as tree is for visual purposes only)
         print_tree(X_test=X_test, y_test=y_test, max_depth=3, feature_names=vars,
                    fontsize=8, save_path=save_path, save_name="example_dt_structure")
 
     run_time = dt.datetime.now() - start_time
     print(f'Finished! Run time: {run_time}')
-# todo: requirements file
