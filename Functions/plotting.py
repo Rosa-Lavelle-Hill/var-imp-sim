69e7453c195a81fe84cdf53d324f65497b752d91
Rosa Lavelle-Hill <rosaellenlavellehill@gmail.com>
2025-04-02 13:31:27 +0200

    add small changes to y-axis of PDP to align with ALE for Figure 1

--- Functions/plotting.py
+++ Functions/plotting.py
@@ -212,10 +212,16 @@ def plot_PDP_orig(pred_model, model, X_test, features, save_path,
     plt.savefig(save_path + f'{pred_model}_{save_name}.png', bbox_inches='tight')
     return
 
-def plot_PDP(pred_model, model, X_test, features, save_path,
-             save_name="pdp", figsize=(4, 3.5), ylim=None, label_size=12):
+def plot_PDP(pred_model, model, X_test, features, save_path, hide_deciles=True,
+             save_name="pdp", figsize=(6, 4), ylim=[-2, -1, 0, 1, 2], label_size=12):
     """
     Plots a partial dependence plot and overlays a rug plot for data density.
+    :param pred_model: string containing name of prediction model
+    :param model: the specified model
+    :param X_test: dataframe of data to be explained
+    :param features: a list containing either single features or a tuple of two features
+    :param figsize: size of figure as a tuple
+    :param save_path: file path where plot should be saved
     """
 
     fig, ax = plt.subplots(figsize=figsize)
@@ -223,9 +229,10 @@ def plot_PDP(pred_model, model, X_test, features, save_path,
     # Plot PDP
     display = PartialDependenceDisplay.from_estimator(model, X_test,
                                                       features, ax=ax)
-    # Hide decile lines
-    if hasattr(display, "deciles_vlines_"):
-        plt.setp(display.deciles_vlines_, visible=False)
+    if hide_deciles==True:
+        # Hide decile lines
+        if hasattr(display, "deciles_vlines_"):
+            plt.setp(display.deciles_vlines_, visible=False)
 
     # If it's a single feature, overlay a rug plot
     if len(features) == 1:
@@ -238,57 +245,13 @@ def plot_PDP(pred_model, model, X_test, features, save_path,
         ax.set_xlabel(feature, fontsize=label_size)
 
         if ylim is not None:
-            ax.set_ylim(ylim)
+            ax.set_yticks(ylim)
+            ax.set_yticklabels(['-2', '-1', '0', '1', '2'])
 
     plt.tight_layout()
     plt.savefig(save_path + f'{pred_model}_{save_name}.png', bbox_inches='tight')
     return
-# def plot_PDP(pred_model, model, X_test, features, save_path,
-#              save_name="pdp", figsize=(4, 3.5), ylim=None, label_size=12):
-#     """
-#     Plots a partial dependence plot and overlays a rug plot for data density.
-#     :param pred_model: string containing name of prediction model
-#     :param model: the specified model
-#     :param X_test: dataframe of data to be explained
-#     :param features: a list containing either single features or a tuple of two features
-#     :param figsize: size of figure as a tuple
-#     :param save_path: file path where plot should be saved
-#     """
-#     fig, ax = plt.subplots(figsize=figsize)
-#
-#     # Plot PDP
-#     display = PartialDependenceDisplay.from_estimator(model, X_test, features, ax=ax)
-#
-#     # Remove vertical dashed decile lines
-#     for line in ax.lines:
-#         xdata = line.get_xdata()
-#         linestyle = line.get_linestyle()
-#         alpha = line.get_alpha()
-#
-#         if len(xdata) == 2 and xdata[0] == xdata[1]:
-#             if linestyle == '--' and (alpha is None or alpha < 0.7):
-#                 line.remove()
-#
-#     # If it's a single feature, overlay a rug plot
-#     if len(features) == 1:
-#         feature = features[0]
-#         ax = display.axes_[0, 0]  # Get the axis used by PDP
-#
-#         # Add rug plot using seaborn or matplotlib
-#         sns.rugplot(x=X_test[feature], ax=ax, color='black', alpha=0.2, height=0.025)
-#
-#         ax.set_ylabel('Average Prediction of y', fontsize=label_size)
-#         ax.set_xlabel(feature, fontsize=label_size)
-#
-#         if ylim is not None:
-#             ax.set_ylim(ylim)
-#
-#     plt.tight_layout()
-#     plt.savefig(save_path + f'{pred_model}_{save_name}.png', bbox_inches='tight')
-#     plt.clf()
-#     plt.cla()
-#     plt.close()
-#     return
+
 
 
 def plot_ICE_deciles(pred_model, model, X_test, feature, save_path, figsize=(8, 3.5), ylab=None, xlab=None,
